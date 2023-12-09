813d335f1d7537743ed5082aae27d6a13d0f1667
eleilop <eloyleivalopez48@gmail.com>
2023-12-09 16:29:27 +0100

    EJ01

---
+++ EJ01/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] num = new int[12];
+        num[0] = 39;
+        num[1] = -2;
+        num[4] = 0;
+        num[6] = 14;
+        num[8] = 5;
+        num[9] = 120;
+        for (int i=0;i<12;i++) 
+            System.out.printf("Índice %d = %d%n", i, num[i]);
+        System.out.println("Como podemos observar, los valores de los elementos no inicializados es 0.");
+    }
+}
